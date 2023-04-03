85d3d62ae99f82de072242de078d2e584fe0ebe9
rvogel <vogel@hallowelt.biz>
2023-04-03 14:06:05 +0200

    Fix I18N
    Change-Id: Ie72515ac6df60fb164d261ce340d010468b098c3

---
+++ languages/ContentStabilization.special.alias.php
@@ -0,0 +1,13 @@
+<?php
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'ContentStabilization' => [ 'Quality management overview', 'ContentStabilization' ],
+];
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = [
+	'ContentStabilization' => [ 'Qualitätsmanagement Überblick' ],
+];
