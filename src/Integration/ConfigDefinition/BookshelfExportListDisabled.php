e9c0a7acfcce9fa775a397ff8785bd72bf49e2b1
d.savuljesku <d.savuljesku@gmail.com>
2023-07-07 13:31:46 +0200

    Fix evaluation of Bookshelf stabilization variables
    [4.3.x]
    
    ERM33033
    
    Change-Id: I8cf707fcb762d2b4c6c75266227eb69619c38156
    (cherry picked from commit ac07ac7d0bdc1699dcc8e8b131717941d1b3be78)

--- src/Integration/ConfigDefinition/BookshelfExportListDisabled.php
+++ src/Integration/ConfigDefinition/BookshelfExportListDisabled.php
@@ -4,9 +4,10 @@ namespace MediaWiki\Extension\ContentStabilization\Integration\ConfigDefinition;
 
 use BlueSpice\Bookshelf\ISettingPaths;
 use BlueSpice\ConfigDefinition\BooleanSetting;
+use BlueSpice\ConfigDefinition\IOverwriteGlobal;
 use ExtensionRegistry;
 
-class BookshelfExportListDisabled extends BooleanSetting implements ISettingPaths {
+class BookshelfExportListDisabled extends BooleanSetting implements ISettingPaths, IOverwriteGlobal {
 
 	/**
 	 *
@@ -40,5 +41,12 @@ class BookshelfExportListDisabled extends BooleanSetting implements ISettingPath
 	 */
 	public function isHidden() {
 		return !ExtensionRegistry::getInstance()->isLoaded( 'BlueSpiceBookshelf' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getGlobalName() {
+		return 'wgBlueSpiceBookshelfExportListDisabled';
 	}
 }
