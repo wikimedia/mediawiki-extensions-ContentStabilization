555807ad350d56babb97fae5564daabe6080c6b2
d.savuljesku <d.savuljesku@gmail.com>
2023-08-10 15:45:10 +0200

    Migrate "enabled namespaces" from nm-settings and dynamic config
    [4.3.x]
    
    ERM 33729
    
    Change-Id: I231bb662d4a40de8d9994c5c1a333fb2e533a73f

---
+++ src/Migration/MigrateNamespaceSettings.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MediaWiki\Extension\ContentStabilization\Migration;
+
+use LoggedUpdateMaintenance;
+use MediaWiki\MediaWikiServices;
+
+require_once dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) . '/maintenance/Maintenance.php';
+
+class MigrateNamespaceSettings extends LoggedUpdateMaintenance {
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function doDBUpdates() {
+		$this->output( 'Migrate FlaggedRevs namespace settings... ' );
+
+		$lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
+
+		$nsSettings = new NamespaceSettings( $lb );
+		$nsStatus = $nsSettings->migrate();
+		if ( !$nsStatus->isOK() ) {
+			$this->output( "failed\n" );
+			return false;
+		}
+		$this->output( "done\n" );
+		return true;
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getUpdateKey() {
+		return 'ContentStabilization::MigrateFlaggedRevsNamespaceSettings';
+	}
+}
+
+$maintClass = MigrateFlaggedRevsData::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
