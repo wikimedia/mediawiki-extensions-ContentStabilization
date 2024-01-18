775213bbdf04cdee2a5d3d409dd8ca37188972cc
d.savuljesku <d.savuljesku@gmail.com>
2024-01-18 10:24:45 +0100

    Fix issues when page is in virtual NS (eg. NS_MEDIA)
    [4.4.x]
    
    ERM34927
    
    Change-Id: Ia49aff25cd807dba987f2f86ee532e975192a4f2

--- src/Hook/StabilizeContent.php
+++ src/Hook/StabilizeContent.php
@@ -289,7 +289,7 @@ class StabilizeContent implements
 	 * @inheritDoc
 	 */
 	public function onImagePageFindFile( $page, &$file, &$displayFile ) {
-		if ( !$this->lookup->isStabilizationEnabled( $page->getTitle() ) ) {
+		if ( !$this->lookup->isStabilizationEnabled( $page->getContext()->getTitle() ) ) {
 			return;
 		}
 
