e0524347ca642523ca022328983bdd33847641c6
dvogel <daniel.vogel@hallowelt.com>
2024-02-05 09:05:24 +0100

    Fix parser clear state
    [4.4.x]
    
    ERM34927
    
    Change-Id: I67342800a7ec3327d251f6ae3501daf45fcabca2

--- src/InclusionManager.php
+++ src/InclusionManager.php
@@ -200,7 +200,7 @@ class InclusionManager {
 		$parserOptions = $page->makeParserOptions( 'canonical' );
 		$parser = $this->parserFactory->create();
 		// Only clear state if the Parser is not in the middle of parsing already (called recursively)
-		$clearState = $parser->getOutput() === null;
+		$clearState = $parser->getOutput() === null || $parser->getStripState() === null;
 		$parserOutput = $parser->parse(
 			$page->getContent()->getWikitextForTransclusion(), $page->getTitle(), $parserOptions,
 			true, $clearState, $page->getTitle()->getLatestRevID()
