a45cea16484b82627672764894b987a499fd11c1
hslater <hamish.slater@hallowelt.com>
2024-04-10 22:36:00 +1200

    Convert to abstract schema
    ERM36733
    
    [REL1_39, REL1_39-2.0.x, master]
    
    Change-Id: I4f30b5f3c673283644053563d061a1c214ddae89
    (cherry picked from commit c9aa61621504c8048880d10c361e1ce445f0d8b5)

--- db/stable_file_transclusions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/stable_file_transclusions (
-	`sft_revision` INT NOT NULL,
-	`sft_page` INT NOT NULL,
-	`sft_file_revision` INT NOT NULL,
-	`sft_file_name` VARCHAR(255) NOT NULL,
-	`sft_file_timestamp` VARBINARY(14) NOT NULL,
-	`sft_file_sha1` VARBINARY(32) NOT NULL
-	) /*$wgDBTableOptions*/;
-CREATE UNIQUE INDEX `sft_revision_file_name_timestamp` ON /*$wgDBprefix*/stable_file_transclusions (`sft_revision`, `sft_file_name`, `sft_file_timestamp` );
