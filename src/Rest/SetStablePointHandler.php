b74f46834c29bd8da34cdad159761fc5e4aef18d
Zoranzoki21 <zorandori4444@gmail.com>
2024-07-30 11:49:49 +0000

    Revert "Use body parameters instead of post parameters"
    This reverts commit cebba0d22f56a1c979d5f05fc320c1697cb13965.
    
    Reason for revert: Not compatible with MediaWiki 1.39
    
    Change-Id: I2012541cf062ca7433803ab09b073bbb0edd9a44

--- src/Rest/SetStablePointHandler.php
+++ src/Rest/SetStablePointHandler.php
@@ -7,6 +7,8 @@ use MediaWiki\Extension\ContentStabilization\StabilizationLookup;
 use MediaWiki\Extension\ContentStabilization\StablePoint;
 use MediaWiki\Rest\HttpException;
 use MediaWiki\Rest\Response;
+use MediaWiki\Rest\Validator\BodyValidator;
+use MediaWiki\Rest\Validator\JsonBodyValidator;
 use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Revision\RevisionStore;
 use TitleFactory;
@@ -67,16 +69,21 @@ class SetStablePointHandler extends StabilizerHandler {
 		return $this->getValidatedBody()['page'];
 	}
 
-	public function getBodyParamSettings(): array {
-		return [
-			'comment' => [
-				self::PARAM_SOURCE => 'body',
-				ParamValidator::PARAM_REQUIRED => false,
-			],
-			'page' => [
-				self::PARAM_SOURCE => 'body',
-				ParamValidator::PARAM_REQUIRED => true,
-			],
-		];
+	/**
+	 * @param string $contentType
+	 * @return BodyValidator
+	 */
+	public function getBodyValidator( $contentType ) {
+		if ( $contentType === 'application/json' ) {
+			return new JsonBodyValidator( [
+				'comment' => [
+					ParamValidator::PARAM_REQUIRED => false,
+				],
+				'page' => [
+					ParamValidator::PARAM_REQUIRED => true,
+				],
+			] );
+		}
+		return parent::getBodyValidator( $contentType );
 	}
 }
