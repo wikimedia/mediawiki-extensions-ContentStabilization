8a8e79224c877fd6e154f22b06827cd3a51e6c4f
hslater <hamish.slater@hallowelt.com>
2024-09-24 16:43:33 +1200

    Convert Special:PageAssignments to OOJS
    ERM36351
    
    [REL1_39, REL1_39-2.1.x, master]
    
    Depends-On: I755b63b9ca36719cceea100c0bc404c9f6d12398
    Change-Id: Icbb14bbeba5ed6025b54bb781134f756e8bc2378

--- resources/pageassignments.addStabilizationColumn.js
+++ resources/pageassignments.addStabilizationColumn.js
@@ -1,24 +1,21 @@
-$( document ).on( 'BSPageAssignmentsOverviewPanelInit', function ( e, sender, cols, fields, actions ) {
-	fields.push( 'last_stable_date' );
+( ( mw ) => {
 
-	cols.push( {
-		text: mw.message( 'contentstabilization-column-last-stable' ).plain(),
-		xtype: 'datecolumn',
-		format: 'Y-m-d H:i', // Doesn't work with custom renderer :(
-		dataIndex: 'last_stable_date',
-		sortable: true,
-		filter: {
-			type: 'date'
-		},
-		renderer: function ( value, metaData, record, rowIndex, colIndex, store, view ) {
-			if ( !value ) {
-				return '<em>' + mw.message( 'contentstabilization-no-stable' ).plain() + '</em>';
+	mw.hook( 'BSPageAssignmentsOverviewPanelInit' ).add( ( gridCfg ) => {
+		gridCfg.columns.last_stable_date = {
+			headerText: mw.message( 'contentstabilization-column-last-stable' ).plain(),
+			sortable: true,
+			filter: { type: 'date' },
+			valueParser: ( val ) => {
+				if ( !val ) {
+					return mw.message( 'contentstabilization-no-stable' ).plain();
+				}
+
+				const date = Ext.Date.parse( val, 'YmdHis' ),
+					dateRenderer = Ext.util.Format.dateRenderer( 'Y-m-d, H:i' );
+
+				return dateRenderer( date );
 			}
-
-			const date = Ext.Date.parse( value, 'YmdHis' ),
-				renderer = Ext.util.Format.dateRenderer( 'Y-m-d, H:i' );
-
-			return renderer( date );
-		}
+		};
 	} );
-} );
+
+} )( mediaWiki );
