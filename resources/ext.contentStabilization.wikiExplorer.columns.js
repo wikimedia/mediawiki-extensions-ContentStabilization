80d9fda469b3414e234a45570b04b511355e7757
akulbii <alk@astarios.com>
2024-10-07 03:36:54 +0300

    Integrate approval columns into WikiExplorer
    [4.5.x]
    
    ERM36382
    
    Depends-On: I4218b4a51db63cbce6557fc8f09097b96860d175
    Change-Id: I988045c3dd70c33f00eac47930954772e61ad160

---
+++ resources/ext.contentStabilization.wikiExplorer.columns.js
@@ -0,0 +1,73 @@
+mw.hook( 'bs.wikiexplorer.oojs.columns' ).add( function ( columns ) {
+	columns.contentstabilization_state = {
+		headerText: mw.message( 'contentstabilization-wikiexplorer-state' ).text(),
+		type: 'boolean',
+		valueParser: function ( val, row ) {
+			if (
+				!row.hasOwnProperty( 'is_contentstabilization_enabled' ) ||
+				row.is_contentstabilization_enabled === false
+			) {
+				return null;
+			}
+
+			return val;
+		},
+		filter: {
+			type: 'boolean'
+		},
+		sortable: true
+	};
+
+	columns.contentstabilization_date = {
+		headerText: mw.message( 'contentstabilization-wikiexplorer-date' ).text(),
+		type: 'date',
+		valueParser: function ( val, row ) {
+			if (
+				!row.hasOwnProperty( 'is_contentstabilization_enabled' ) ||
+				row.is_contentstabilization_enabled === false
+			) {
+				return null;
+			}
+
+			if ( val === null ) {
+				return '';
+			}
+
+			// MW to ISO
+			// YYYYMMDDHHMMSS => YYYY-MM-DDTHH
+			const match = val.match(
+				/^(\d{4})(\d{2})(\d{2}).*$/
+			);
+			const date = new Date( match[ 1 ] + '-' + match[ 2 ] + '-' + match[ 3 ] );
+			if ( !date ) {
+				return '';
+			}
+
+			return date.toLocaleDateString();
+		},
+		filter: {
+			type: 'date'
+		},
+		hidden: true,
+		sortable: true
+	};
+
+	columns.contentstabilization_is_new_available = {
+		headerText: mw.message( 'contentstabilization-wikiexplorer-is-new-available' ).text(),
+		type: 'boolean',
+		valueParser: function ( val, row ) {
+			if (
+				!row.hasOwnProperty( 'is_contentstabilization_enabled' ) ||
+				row.is_contentstabilization_enabled === false
+			) {
+				return null;
+			}
+
+			return val;
+		},
+		filter: {
+			type: 'boolean'
+		},
+		sortable: true
+	};
+} );
