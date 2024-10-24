15c107c1a2ac204aeed88078a99281726f947c5e
d.savuljesku <d.savuljesku@gmail.com>
2024-10-24 10:33:28 +0200

    Integrate into NotifyMe - prevent notifications for draft page if user cannot see drafts
    [4.5.x]
    
    ERM34657
    
    Change-Id: Id51c1b5d8fc4fb5f89edc37e7baa8bfe38176554
    (cherry picked from commit df31a88f8eb7fefd52de62bd67cbf245c0a38a17)
    (cherry picked from commit fc56092e4658bd2bf23330a69727c98c934d16fc)

---
+++ src/Integration/Hook/StabilizeNotifications.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace MediaWiki\Extension\ContentStabilization\Integration\Hook;
+
+use MediaWiki\Extension\ContentStabilization\StabilizationLookup;
+use MediaWiki\Extension\NotifyMe\Hook\NotifyMeBeforeGenerateNotificationHook;
+use MediaWiki\User\UserIdentity;
+use MWStake\MediaWiki\Component\Events\INotificationEvent;
+use MWStake\MediaWiki\Component\Events\ITitleEvent;
+
+class StabilizeNotifications implements NotifyMeBeforeGenerateNotificationHook {
+
+	/**
+	 * @var StabilizationLookup
+	 */
+	private $lookup;
+
+	/**
+	 * @param StabilizationLookup $lookup
+	 */
+	public function __construct( StabilizationLookup $lookup ) {
+		$this->lookup = $lookup;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onNotifyMeBeforeGenerateNotification(
+		INotificationEvent $event, UserIdentity $forUser, array $providers, bool &$prevent
+	): bool {
+		if ( !( $event instanceof ITitleEvent ) ) {
+			return true;
+		}
+		$title = $event->getTitle();
+		if ( !$this->lookup->isStabilizationEnabled( $title ) ) {
+			return true;
+		}
+		if ( $this->lookup->canUserSeeUnstable( $forUser ) ) {
+			return true;
+		}
+		$isDraft = !empty( $this->lookup->getPendingUnstableRevisions( $title ) );
+		$isFirstDraft = $this->lookup->getLastStablePoint( $title ) === null;
+		if ( $isFirstDraft && $this->lookup->isFirstUnstableAllowed() ) {
+			return true;
+		}
+		if ( $isDraft ) {
+			$prevent = true;
+			return false;
+		}
+		return true;
+	}
+}
