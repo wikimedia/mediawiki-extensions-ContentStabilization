4ff70b5a972b2ad5bb5ec3d177a5bf95188aeb31
d.savuljesku <d.savuljesku@gmail.com>
2024-11-26 13:33:59 +0100

    Allow removing approvals in page history grid
    Depends-On: I48ba059e671779be3b3ddf8bc174590705088b1c
    
    [4.5.x]
    
    ERM39550
    
    Change-Id: I95333c66a68830dae137d68e144fe86ecadb70af
    (cherry picked from commit d153a11c1f3a2daa5cd2ee944755edd0440f815f)

--- resources/ext.contentStabilization.enhancedHistory.js
+++ resources/ext.contentStabilization.enhancedHistory.js
@@ -31,6 +31,31 @@
 			sortable: false,
 			hidden: !mw.user.options.get( 'history-show-sp_approve_comment' )
 		};
+		gridCfg.columns.removeApproval = {
+			type: 'remove-approval',
+			title: mw.message( 'contentstabilization-versionhistory-grid-header-remove-approval-title' ).text(),
+			actionId: 'removeApproval',
+			icon: 'unFlag'
+		};
+	} );
+
+	mw.hook( 'enhanced.versionhistory.rowAction' ).add( function( action, row ) {
+		if ( action === 'removeApproval' && row.sp_approved ) {
+			var revId = parseInt( row.id );
+			OO.ui.confirm( mw.message( 'contentstabilization-versionhistory-remove-approval-confirm' ).text() ).done(
+				function ( confirmed ) {
+					if ( !confirmed ) {
+						return;
+					}
+					$.ajax( {
+						url: mw.util.wikiScript( 'rest' ) + '/content_stabilization/' + revId,
+						type: 'DELETE',
+						success: function () {
+							location.reload();
+						}
+					} );
+				} );
+		}
 	} );
 
 }( mediaWiki ) );
