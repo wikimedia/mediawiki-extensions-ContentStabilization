86bc81e8240806fafb30397745c31a860ff48846
Daniel Vogel <daniel.vogel@hallowelt.com>
2025-01-31 11:56:09 +0100

    Fix styleblock provider
    ERM38695
    
    [5.x]
    
    Change-Id: I0091ba39d01793be916a06d9abe45c414357041d

--- src/Integration/PDFCreator/StyleBlockProvider/CSStyleBlocks.php
+++ src/Integration/PDFCreator/StyleBlockProvider/CSStyleBlocks.php
@@ -3,14 +3,16 @@
 namespace MediaWiki\Extension\ContentStabilization\Integration\PDFCreator\StyleBlockProvider;
 
 use MediaWiki\Extension\PDFCreator\Interface\IStyleBlocksProvider;
+use MediaWiki\Extension\PDFCreator\Utility\ExportContext;
 
 class CSStyleBlocks implements IStyleBlocksProvider {
 
 	/**
 	 * @param string $module
+	 * @param ExportContext $context
 	 * @return array
 	 */
-	public function execute( string $module ): array {
+	public function execute( string $module, ExportContext $context ): array {
 		$base = dirname( __DIR__, 4 ) . '/resources';
 		$styles = file_get_contents( "$base/stabilized-export.css" );
 		if ( !$styles ) {
