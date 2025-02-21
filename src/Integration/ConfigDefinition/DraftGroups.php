cb104634dbcd7d8dff7e47f2464f5e90cd8b4b42
d.savuljesku <d.savuljesku@gmail.com>
2025-02-21 12:19:54 +0100

    Align group options for DraftGroups config option with other places in UI
    [5.x]
    
    ERM40862
    
    Change-Id: Iefe2e546e8ca0cd8d503fe72809dee49101c31c3

--- src/Integration/ConfigDefinition/DraftGroups.php
+++ src/Integration/ConfigDefinition/DraftGroups.php
@@ -2,10 +2,36 @@
 
 namespace MediaWiki\Extension\ContentStabilization\Integration\ConfigDefinition;
 
+use BlueSpice\ConfigDefinition;
 use BlueSpice\ConfigDefinition\ArraySetting;
 use BlueSpice\ConfigDefinition\IOverwriteGlobal;
+use BlueSpice\Utility\GroupHelper;
+use Config;
+use MediaWiki\Context\IContextSource;
+use MediaWiki\MediaWikiServices;
 
 class DraftGroups extends ArraySetting implements IOverwriteGlobal {
+
+	/**
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param string $name
+	 * @return ConfigDefinition|static
+	 */
+	public static function getInstance( $context, $config, $name ) {
+		$groupHelper = MediaWikiServices::getInstance()->getService( 'BSUtilityFactory' )->getGroupHelper();
+		return new static( $context, $config, $name, $groupHelper );
+	}
+
+	/**
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param string $name
+	 * @param GroupHelper $groupHelper
+	 */
+	public function __construct( $context, $config, $name, private readonly GroupHelper $groupHelper ) {
+		parent::__construct( $context, $config, $name );
+	}
 
 	/**
 	 *
@@ -31,21 +57,9 @@ class DraftGroups extends ArraySetting implements IOverwriteGlobal {
 	 * @return array
 	 */
 	public function getOptions() {
-		$excludeGroups = [
-			'bot',
-			'autoconfirmed',
-			'checkuser',
-			'sysop',
-			'reviewer'
-		];
-		$options = [];
-		foreach ( $GLOBALS['wgGroupPermissions'] as $group => $permissions ) {
-			if ( in_array( $group, $excludeGroups ) ) {
-				continue;
-			}
-			$options[] = $group;
-		}
-		return $options;
+		return $this->groupHelper->getGroupsForDisplay( [
+			'blacklist' => [ 'sysop', 'reviewer' ]
+		] );
 	}
 
 	/**
