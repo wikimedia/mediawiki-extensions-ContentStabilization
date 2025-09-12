bae70dfeafe8b77e7131ed5453fdf6d804a7f642
d.savuljesku <d.savuljesku@gmail.com>
2025-09-12 15:48:34 +0200

    Add integration for WikiRAG
    Change-Id: I17c1e9ff5033742c91ddf342dd377cbfd9e02879

---
+++ src/Integration/ConfigDefinition/WikiRAGIndexOnlyStable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MediaWiki\Extension\ContentStabilization\Integration\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition\BooleanSetting;
+use BlueSpice\ConfigDefinition\IOverwriteGlobal;
+use MediaWiki\Registration\ExtensionRegistry;
+
+class WikiRAGIndexOnlyStable extends BooleanSetting implements IOverwriteGlobal {
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_QUALITY_ASSURANCE . "/ContentStabilization",
+			static::MAIN_PATH_EXTENSION . '/ContentStabilization/' . static::FEATURE_QUALITY_ASSURANCE,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_PRO . "/ContentStabilization",
+		];
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return 'contentstabilization-pref-wiki-rag-index-stable';
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getGlobalName() {
+		return "wgWikiRAGIndexOnlyStable";
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isHidden() {
+		return !ExtensionRegistry::getInstance()->isLoaded( 'WikiRAG' );
+	}
+}
