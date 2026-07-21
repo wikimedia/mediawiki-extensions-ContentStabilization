8c1b0f9f76c0697f1bb62a67ebdc4ca313f19639
driedmueller <daniel.riedmueller@hallowelt.com>
2026-07-21 14:42:02 +0200

    Auto approve page move, if previously approved
    [5.1.x]
    [ERM49121]
    
    Change-Id: I15c4eb5775c2d6a1dc4c0295365ecb6a30a7260a

--- src/Hook/AutoStabilize.php
+++ src/Hook/AutoStabilize.php
@@ -2,35 +2,29 @@
 
 namespace MediaWiki\Extension\ContentStabilization\Hook;
 
+use MediaWiki\Content\Hook\ContentAlterParserOutputHook;
 use MediaWiki\Context\RequestContext;
 use MediaWiki\Extension\ContentStabilization\ContentStabilizer;
 use MediaWiki\Extension\ContentStabilization\StabilizationLookup;
+use MediaWiki\Revision\RevisionStore;
 use MediaWiki\Storage\Hook\PageSaveCompleteHook;
 use Psr\Log\LoggerInterface;
 use Throwable;
 
-class AutoStabilize implements PageSaveCompleteHook {
-
-	/**
-	 * @var ContentStabilizer
-	 */
-	private $stabilizer;
-
-	/** @var StabilizationLookup */
-	private $lookup;
-
-	/** @var LoggerInterface */
-	private $logger;
+class AutoStabilize implements PageSaveCompleteHook, ContentAlterParserOutputHook {
 
 	/**
 	 * @param ContentStabilizer $stabilizer
 	 * @param StabilizationLookup $lookup
+	 * @param RevisionStore $revisionStore
 	 * @param LoggerInterface $logger
 	 */
-	public function __construct( ContentStabilizer $stabilizer, StabilizationLookup $lookup, LoggerInterface $logger ) {
-		$this->stabilizer = $stabilizer;
-		$this->lookup = $lookup;
-		$this->logger = $logger;
+	public function __construct(
+		private readonly ContentStabilizer $stabilizer,
+		private readonly StabilizationLookup $lookup,
+		private readonly RevisionStore $revisionStore,
+		private readonly LoggerInterface $logger
+	) {
 	}
 
 	/**
@@ -47,7 +41,10 @@ class AutoStabilize implements PageSaveCompleteHook {
 		}
 		try {
 			$this->stabilizer->addStablePoint(
-				$revisionRecord, RequestContext::getMain()->getUser(), '', ContentStabilizer::VALIDATION_IGNORE_CURRENT
+				$revisionRecord,
+				RequestContext::getMain()->getUser(),
+				'',
+				ContentStabilizer::VALIDATION_IGNORE_CURRENT
 			);
 		} catch ( Throwable $ex ) {
 			$this->logger->error( 'Failed to auto-stabilize page: {exception}', [
@@ -55,5 +52,51 @@ class AutoStabilize implements PageSaveCompleteHook {
 			] );
 			// Ignore - do not break a save
 		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onContentAlterParserOutput( $content, $title, $parserOutput ) {
+		if ( !$this->lookup->isStabilizationEnabled( $title ) ) {
+			return;
+		}
+
+		$context = RequestContext::getMain();
+		if ( $context->getRequest()->getText( 'action' ) !== "move" ) {
+			return;
+		}
+
+		$lastStablePoint = $this->lookup->getLastStablePoint( $title );
+		if ( !$lastStablePoint ) {
+			return;
+		}
+
+		$selected = $this->revisionStore->getRevisionByTitle( $title );
+		if ( !$selected ) {
+			return;
+		}
+
+		$isStable = $this->lookup->isStableRevision( $selected );
+		if ( $isStable ) {
+			return;
+		}
+
+		$previous = $this->revisionStore->getPreviousRevision( $selected );
+		if ( !$previous ) {
+			return;
+		}
+
+		$isPreviousStable = $this->lookup->isStableRevision( $previous );
+		if ( !$isPreviousStable ) {
+			return;
+		}
+
+		$this->stabilizer->moveStablePointUnsafe(
+			$lastStablePoint,
+			$selected,
+			$lastStablePoint->getApprover(),
+			$lastStablePoint->getComment()
+		);
 	}
 }
